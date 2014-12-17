20cce0fa14eec16efec1ea73b478fe2e50cfade4
jruberg <jruberg@braig.local>
2014-12-17 07:11:59 -0800

    more

---
+++ js/poll.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  image_table = {
+    ep7digmonholdstillyoupile: 'rdd_202_scrap_metal',
+    ep8karihesjustakidlikeyou: 'rdd_202_nice_friends',
+    ep8kariwhereishetkizzynow: 'rdd_202_izzy',
+    ep8kariwhatsitdoingthereg: 'rdd_202_control_spire',
+    ep9yoleilookathisiqhecoul: 'rdd_202_program_my_computer',
+    ep10daviswheresyoleiizzys: 'rdd_202_glasses',
+    ep10tentomonwelcometomytr: 'rdd_202_twig_house',
+    ep10kenhowdidagumongetawa: 'rdd_202_escape_clause'
+  }
+
+  $('.PDS_Poll .pds-answer-span').each(function(i, elem) {
+    answer_text = $(elem).text();
+    answer_key = answer_text.replace(/\W/g, '').toLowerCase().slice(0, 25);
+    answer_name = image_table[answer_key];
+    if (answer_name) {
+      answer_file = '/img/' + answer_name + '.png';
+      // $(elem).text('');
+      img = $('<img class="answer_img" src="' + answer_file + '" />');
+      $(elem).append(img);
+    }
+  });
+});
