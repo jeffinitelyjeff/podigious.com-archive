c8e9f4827a0fcc3bda688300965f66ecd2f8f429
Jeff Ruberg <rubergly@gmail.com>
2015-08-21 06:08:48 -0700

    more

--- assets/podigious.js
+++ assets/podigious.js
@@ -1,16 +1,32 @@
+var make_new_path = function(parts, original_name, extension) {
+  parts[parts.length - 2] = original_name + extension;
+  return parts.join('.');
+}
+
+var random_element = function(arr) {
+  return arr[Math.floor(Math.random()*arr.length)];
+}
+
 $(document).ready(function() {
   $('.altable-banner').each(function() {
     var $img = $(this).find('img');
-    var src = $img.attr('src');
-    var parts = src.split('.');
-    parts[parts.length - 2] = parts[parts.length - 2] + '_alt';
-    var new_src = parts.join('.');
-    console.log(src);
-    console.log(new_src);
+    var old_src = $img.attr('src');
+    var parts = old_src.split('.');
+    var original_name = parts[parts.length - 2];
+    var new_srcs = [];
+    if ($(this).data('banners')) {
+      for (var i = 0; i < $(this).data('banners'); i++) {
+        new_srcs.push(make_new_path(parts, original_name, '_alt' + i));
+      }
+    } else {
+      new_srcs.push(make_new_path(parts, original_name, '_alt'));
+    }
+    console.log(old_src);
+    console.log(new_srcs);
     $(this).hover(function() {
-      $img.attr('src', new_src);
+      $img.attr('src', random_element(new_srcs));
     }, function() {
-      $img.attr('src', src);
+      $img.attr('src', old_src);
     });
   });
 });
