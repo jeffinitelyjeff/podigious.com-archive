e441c51723a898084bdcfac0873323ec83e22534
Jeff Ruberg <rubergly@gmail.com>
2016-08-29 03:27:19 -0700

    more

--- assets/podigious.js
+++ assets/podigious.js
@@ -29,4 +29,13 @@ $(document).ready(function() {
       $img.attr('src', old_src);
     });
   });
+
+  $('.shuffle-banner').shuffleImages({
+    trigger: "imageMouseMove",
+    triggerTarget: false,
+    mouseMoveTrigger: 50,
+    hoverTrigger: 200,
+    scrollTrigger: 100,
+    target: "> img"
+  });
 });
